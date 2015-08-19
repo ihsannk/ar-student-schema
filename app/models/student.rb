98c63f93a1b437183f87f554efbcbc060b799ce7
Jeremy <jeremy.ongws@gmail.com>
2015-08-19 18:56:08 +0800

    WIP

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,19 @@
 require_relative '../../db/config'
 
 class Student < ActiveRecord::Base
-# implement your Student model here
+  validates :email, uniqueness: true,format: { with: /\w+@\w+(.\w+)*(\.\w{2,})/ }
+  validates :age, numericality: { greater_than_or_equal_to: 5 }
+  validates :phone, format: { with: /.+\d{3}.+\d{3}.+\d{4}.+/ }
+
+  def name
+    "#{first_name} #{last_name}"
+  end
+
+  def age
+    today = Date.today
+    birthyear = self.birthday.year
+    return today.year - birthyear - 1
+  end
+
+
 end
--- db/migrate/20121011144238_create_students.rb
+++ db/migrate/20121011144238_create_students.rb
@@ -1,9 +1,19 @@
 require_relative '../config'
-
-# this is where you should use an ActiveRecord migration to 
-
+# this is where you should use an ActiveRecord migration to
 class CreateStudents < ActiveRecord::Migration
   def change
+    create_table :students do |t|
+    t.string :first_name
+    t.string :last_name
+    t.string :gender
+    t.date :birthday
+    t.string :email
+    t.string :phone
+    t.timestamps null: false
+    end
     # HINT: checkout ActiveRecord::Migration.create_table
   end
 end
+
+# first_name,last_name,gender,birthday,email,phone
+# Kyle,Snell,male,1970-01-01,kyle.snell@example.com,415-555-1212
